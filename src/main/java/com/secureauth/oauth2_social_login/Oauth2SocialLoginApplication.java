ac514bcdbd9abc47dd1b5881771d55108ae0cb98
Nika Oniani <nikaoniani2018@gmail.com>
2025-01-21 19:35:54 +0400

    initial commit

---
+++ src/main/java/com/secureauth/oauth2_social_login/Oauth2SocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.secureauth.oauth2_social_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2SocialLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secureauth/oauth2_social_login/Oauth2SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureauth.oauth2_social_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
