e3bf7a398ad9d5004055dc520d65b610bd21f89e
Nika Oniani <nikaoniani2018@gmail.com>
2025-01-31 22:25:12 +0400

    Add user dashboard page with logout functionality using Thymeleaf

--- src/main/java/com/secureauth/oauth2_social_login/security/SecurityConfig.java
+++ src/main/java/com/secureauth/oauth2_social_login/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.secureauth.oauth2_social_login.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -26,7 +25,8 @@ public class SecurityConfig {
                         .requestMatchers("/otp/create").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
+                .formLogin(login -> login
+                        .defaultSuccessUrl("/dashboard", true))
                 .oauth2Login(oauth2 -> oauth2.successHandler(successHandler))
         ;
 
